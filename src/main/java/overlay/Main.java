40bfe3aa6146ac5e967d8ab8bf3a46f72508b827
Pierre <tournon.pierre1@gmail.com>
2022-04-07 17:15:04 +0200

    Ajout de la classe Dijkstra, nettoyage de la classe Main

--- src/main/java/overlay/Main.java
+++ src/main/java/overlay/Main.java
@@ -11,10 +11,9 @@ import java.util.Map;
 
 public class Main {
 
-    public static Map<Integer, Integer> pred = new HashMap<Integer, Integer>();
-
     public static void main(String[] args) {
-        Map<Integer, List<Integer>> graph = new HashMap<Integer, List<Integer>>();
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
         Path file = Path.of(args[0]);
         try{
             for (String line : Files.readAllLines(file, StandardCharsets.UTF_8)) {
@@ -28,65 +27,6 @@ public class Main {
         }catch(IOException e) {
             e.printStackTrace();
         }
-        Dijkstra(1,4, graph);
     }
-
-    public static ArrayList<Integer> Dijkstra (int source, int target, Map<Integer, List<Integer>> graph) {
-        ArrayList<Integer> shortestPath = new ArrayList<>();
-        Map<Integer, Integer> dists = new HashMap<>();
-
-        //Initialisation des distances
-        for(int d : graph.keySet()) {
-            if(d == source)
-                dists.put(d, 0);
-            else
-                dists.put(d, Integer.MAX_VALUE);
-        }
-        /*for(int e : dists.keySet()) {
-            System.out.println(e+" "+ dists.get(e));
-        }*/
-
-        List<Integer> nodesLi = new ArrayList<Integer>(graph.keySet()); //Liste de tous les noeuds.
-
-        //On applique l'algo de Dijkstra en lui même
-        while (!nodesLi.isEmpty()) {
-            int s1 = findMin(dists, nodesLi);
-            nodesLi.remove(nodesLi.indexOf(s1));
-            for(int s2 : graph.get(s1))
-                updDists(s1, s2, dists);
-        }
-
-        //On remonte la liste pour enfin calculer le plus court chemin
-        while (target != source) {
-            shortestPath.add(target);
-            target = pred.get(target);
-        }
-        shortestPath.add(source);
-
-        for(int e : shortestPath)
-            System.out.println(e);
-
-        return shortestPath;
-    }
-
-    public static int findMin (Map<Integer, Integer> dists, List<Integer> graph) {
-        int minPath = Integer.MAX_VALUE;
-        int node = -1;
-        for(int s : graph)
-            if(dists.get(s) < minPath) {
-                minPath = dists.get(s);
-                node = s;
-            }
-
-        return node;
-    }
-
-    public static void updDists (int nodeA, int nodeB, Map<Integer, Integer> dists) {
-        if(dists.get(nodeB) > dists.get(nodeA) + 1) {
-            dists.replace(nodeB, dists.get(nodeA) + 1);
-            pred.put(nodeB, nodeA);
-        }
-    }
-
 }
 
