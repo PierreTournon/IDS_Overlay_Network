2b3566e9297ba9c6cd47ff777945d8844331e5e5
Pierre <tournon.pierre1@gmail.com>
2022-04-08 09:36:42 +0200

    Association des IDs physiques avec les virtuelles de l'overlay

--- src/main/java/overlay/Main.java
+++ src/main/java/overlay/Main.java
@@ -11,9 +11,10 @@ import java.util.Map;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
+    private static Map<Integer, List<Integer>> graph = new HashMap<>();
+    private static Map<Integer, String> physToVirtu = new HashMap<>();
 
+    public static void main(String[] args) {
         Path file = Path.of(args[0]);
         try{
             for (String line : Files.readAllLines(file, StandardCharsets.UTF_8)) {
@@ -22,11 +23,33 @@ public class Main {
                 for(int i = 0; i < parsedLine.length; i++)
                     list.add(Integer.parseInt(parsedLine[i]));
 
-                graph.put(list.get(0), list.subList(1, list.size()));
+                graph.put(list.get(0), list.subList(1, list.size())); //Construction du graphe.
             }
         }catch(IOException e) {
             e.printStackTrace();
         }
+
+        makeVirtualIds();
     }
+
+    /**
+     * Création des IDs virtuelles associées aux IDs physiques.
+     */
+    private static void makeVirtualIds () {
+        int id = 0;
+        for(int e : graph.keySet()) {
+            physToVirtu.put(e, String.valueOf(0));
+            i++;
+        }
+    }
+
+    public static String getVirtualId (int physicalId) {
+        return physToVirtu.get(physicalId);
+    }
+
+    public static Map<Integer, List<Integer>> getGraph () {
+        return graph;
+    }
+
 }
 
